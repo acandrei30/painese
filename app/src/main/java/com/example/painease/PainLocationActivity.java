11f170380786b2723dc5257b9076009eba62587f
acandrei30 <acandreipp@gmail.com>
2023-08-26 11:47:43 +0300

    v1

--- app/src/main/java/com/example/painease/PainLocationActivity.java
+++ app/src/main/java/com/example/painease/PainLocationActivity.java
@@ -15,7 +15,18 @@ public class PainLocationActivity extends AppCompatActivity {
     private ImageView bodyBase;
     private ImageView bodyBackBase;
     private ImageView headOverlay;
-    private ImageView torsoOverlay;
+    private ImageView throatOverlay;
+    private ImageView chestOverlay;
+    private ImageView stomachOverlay;
+    private ImageView leftarmOverlay;
+    private ImageView rightarmOverlay;
+    private ImageView leftwristOverlay;
+    private ImageView rightwristOverlay;
+    private ImageView coreOverlay;
+    private ImageView rightlegOverlay;
+    private ImageView rightankleOverlay;
+    private ImageView leftlegOverlay;
+    private ImageView leftankleOverlay;
     private ImageButton switchViewButton;
     private Button doneSelectingButton;
 
@@ -24,13 +35,8 @@ public class PainLocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pain_location_page);
 
-        bodyBase = findViewById(R.id.bodyImageViewBase);
-        bodyBackBase = findViewById(R.id.bodyBackImageViewBase);
-        headOverlay = findViewById(R.id.headOverlay);
-        torsoOverlay = findViewById(R.id.torsoOverlay);
-        switchViewButton = findViewById(R.id.switchViewButton);
+        initializeViews();
 
-        doneSelectingButton = findViewById(R.id.doneSelectingButton);
         doneSelectingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,15 +48,7 @@ public class PainLocationActivity extends AppCompatActivity {
         switchViewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (bodyBase.getVisibility() == View.VISIBLE) {
-                    bodyBase.setVisibility(View.INVISIBLE);
-                    bodyBackBase.setVisibility(View.VISIBLE);
-                    clearAllOverlays();
-                } else {
-                    bodyBase.setVisibility(View.VISIBLE);
-                    bodyBackBase.setVisibility(View.INVISIBLE);
-                    clearAllOverlays();
-                }
+                switchBodyView();
             }
         });
 
@@ -58,31 +56,113 @@ public class PainLocationActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    float y = event.getY();
-                    clearAllOverlays();
-                    setOverlayImage(y);
+                    handleTouchOnBody(event.getX(), event.getY());
                 }
                 return true;
             }
         });
+    }
 
-        // TODO: Add touch listener for bodyBackBase to detect touches for the back
+    private void initializeViews() {
+        bodyBase = findViewById(R.id.bodyImageViewBase);
+        bodyBackBase = findViewById(R.id.bodyBackImageViewBase);
+        headOverlay = findViewById(R.id.headOverlay);
+        throatOverlay = findViewById(R.id.throatOverlay);
+        chestOverlay = findViewById(R.id.chestOverlay);
+        stomachOverlay = findViewById(R.id.stomachOverlay);
+        leftarmOverlay = findViewById(R.id.leftarmOverlay);
+        rightarmOverlay = findViewById(R.id.rightarmOverlay);
+        leftwristOverlay = findViewById(R.id.leftwristOverlay);
+        rightwristOverlay = findViewById(R.id.rightwristOverlay);
+        coreOverlay = findViewById(R.id.coreOverlay);
+        rightlegOverlay = findViewById(R.id.rightlegOverlay);
+        rightankleOverlay = findViewById(R.id.rightankleOverlay);
+        leftlegOverlay = findViewById(R.id.leftlegOverlay);
+        leftankleOverlay = findViewById(R.id.leftankleOverlay);
+        switchViewButton = findViewById(R.id.switchViewButton);
+        doneSelectingButton = findViewById(R.id.doneSelectingButton);
+    }
+
+    private void switchBodyView() {
+        if (bodyBase.getVisibility() == View.VISIBLE) {
+            bodyBase.setVisibility(View.INVISIBLE);
+            bodyBackBase.setVisibility(View.VISIBLE);
+            clearAllOverlays();
+        } else {
+            bodyBase.setVisibility(View.VISIBLE);
+            bodyBackBase.setVisibility(View.INVISIBLE);
+            clearAllOverlays();
+        }
     }
 
     private void clearAllOverlays() {
         headOverlay.setVisibility(View.INVISIBLE);
-        torsoOverlay.setVisibility(View.INVISIBLE);
+        throatOverlay.setVisibility(View.INVISIBLE);
+        chestOverlay.setVisibility(View.INVISIBLE);
+        stomachOverlay.setVisibility(View.INVISIBLE);
+        leftarmOverlay.setVisibility(View.INVISIBLE);
+        rightarmOverlay.setVisibility(View.INVISIBLE);
+        leftwristOverlay.setVisibility(View.INVISIBLE);
+        rightwristOverlay.setVisibility(View.INVISIBLE);
+        coreOverlay.setVisibility(View.INVISIBLE);
+        rightlegOverlay.setVisibility(View.INVISIBLE);
+        rightankleOverlay.setVisibility(View.INVISIBLE);
+        leftlegOverlay.setVisibility(View.INVISIBLE);
+        leftankleOverlay.setVisibility(View.INVISIBLE);
         doneSelectingButton.setVisibility(View.INVISIBLE);
     }
-
-    private void setOverlayImage(float y) {
+    private void handleTouchOnBody(float x, float y) {
+        int width = bodyBase.getWidth();
         int height = bodyBase.getHeight();
-        if (y < height * 0.25) {
-            headOverlay.setVisibility(View.VISIBLE);
-            doneSelectingButton.setVisibility(View.VISIBLE);
-        } else if (y < height * 0.5) {
-            torsoOverlay.setVisibility(View.VISIBLE);
-            doneSelectingButton.setVisibility(View.VISIBLE);
+
+        clearAllOverlays();
+
+        // For Upper Body
+        if (y < height * 0.5) {
+            if (y < height * 0.125) { // Top 12.5% is the head
+                headOverlay.setVisibility(View.VISIBLE);
+            } else if (y < height * 0.25) { // Next 12.5% is the throat
+                throatOverlay.setVisibility(View.VISIBLE);
+            } else if (y < height * 0.325) { // For chest, arms based on horizontal position
+                if (x < width * 0.3) {
+                    rightarmOverlay.setVisibility(View.VISIBLE);
+                } else if (x < width * 0.7) {
+                    chestOverlay.setVisibility(View.VISIBLE);
+                } else {
+                    leftarmOverlay.setVisibility(View.VISIBLE);
+                }
+            } else if (y < height * 0.425) { // Stomach area
+                stomachOverlay.setVisibility(View.VISIBLE);
+            } else { // For wrists and core
+                if (x < width * 0.35) {
+                    rightwristOverlay.setVisibility(View.VISIBLE);
+                } else if (x < width * 0.65) {
+                    coreOverlay.setVisibility(View.VISIBLE);
+                } else {
+                    leftwristOverlay.setVisibility(View.VISIBLE);
+                }
+            }
         }
+        // For Lower Body
+        else {
+            if (y < height * 0.75) { // Legs
+                if (x < width * 0.5) {
+                    rightlegOverlay.setVisibility(View.VISIBLE);
+                } else {
+                    leftlegOverlay.setVisibility(View.VISIBLE);
+                }
+            } else { // Ankles
+                if (x < width * 0.5) {
+                    rightankleOverlay.setVisibility(View.VISIBLE);
+                } else {
+                    leftankleOverlay.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+
+        doneSelectingButton.setVisibility(View.VISIBLE);
     }
+
+
+
 }
