ed9d0f3ba518db955731113e27e7837b21293956
acandrei30 <acandreipp@gmail.com>
2023-08-29 10:12:45 +0300

    v1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.airbnb.android:lottie:3.7.1")  // As of my last training cut-off in September 2021. Please check for the latest version.
+
 }
